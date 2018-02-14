a2033354075e2973cbf9c1178e2a733974b4ed60
Matej Ivkovčić <matej.ivkovcic@in2.hr>
2018-02-14 15:08:23 +0100

    pocetna struktura

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,19 +2,8 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
+  
+    <header>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
@@ -26,8 +15,8 @@ const Header = () => (
           Gatsby
         </Link>
       </h1>
-    </div>
-  </div>
+    </header>
+  
 )
 
 export default Header
