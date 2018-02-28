2bd40d6db3877fb9478e49d677008dec584bf28d
Mislav Žabčić <mislavz@in2.hr>
2018-02-28 17:27:41 +0100

    fix css prefixer

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,5 +3,27 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+var extractTextWebpackPlugin = require('extract-text-webpack-plugin');
 
+exports.modifyWebpackConfig = function(config, env) {
 
+  if (env === 'build-css') {
+    config.removeLoader('sass');
+    config.loader('sass', {
+      test: /\.(sass|scss)/,
+      exclude: /\.module\.(sass|scss)$/,
+      loader: extractTextWebpackPlugin.extract(['css?minimize', 'autoprefixer?{browsers:"last 2 versions,ie 9"}', 'sass']),
+    })
+  }
+
+  if (env === 'develop') {
+    config.removeLoader('sass');
+    config.loader('sass', {
+      test: /\.(sass|scss)/,
+      exclude: /\.module\.(sass|scss)$/,
+      loaders: ['style', 'css', 'autoprefixer?{browsers:"last 2 versions,ie 9"}', 'sass'],
+    })
+  }
+
+  return config
+};
